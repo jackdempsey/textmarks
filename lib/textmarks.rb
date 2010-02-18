69747e48c35a4885d6be8e00c3cad40e5e632cf9
Jack Dempsey <jack.dempsey@gmail.com>
2010-02-18 11:39:42 -0500

    build simple methods to easily call api

--- lib/textmarks.rb
+++ lib/textmarks.rb
@@ -3,11 +3,22 @@ require 'httparty'
 class Textmarks
   include HTTParty
   format :json
+
   base_uri 'dev1.api2.textmarks.com'
+  default_params :api_key => ::TEXTMARKS_API_KEY if defined?(::TEXTMARKS_API_KEY)
 
   class << self
-    def echo(str)
-      get('/Test/echo', :str => str)
+    @package = nil
+
+    def method_missing(symbol, *args)
+      if @package
+        result = post "/#{@package}/#{symbol}/", :query => args.first
+        @package = nil
+        result
+      else
+        @package = symbol
+        self
+      end
     end
   end
 end
